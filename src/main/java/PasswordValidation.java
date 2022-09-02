2245aea012e3b73c88242c202edc00bee5441ccc
itsilkerse <itsilkerse@gmail.com>
2022-09-02 12:30:09 +0200

    Second function test

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -2,21 +2,32 @@ public class PasswordValidation {
 
     public static void main(String[] args) {
         System.out.println(lengthOfPassword("dal"));
+        System.out.println("");
+    }
+
+
+    public static String lengthOfPassword(String password) {
+
+
+        if (password.length() >= 8) {
+            return "Password is valid!";
+        } else {
+            return "Password is invalid!";
+        }
+
+    }
+
+    public static boolean isNumber (String isDigit) {
+
+        for(int i = 0; i <= 9; i++ ){
+            String.valueOf(i);
+            if(isDigit.contains(i)))
+
+            }
+    }
+
 
     }
 
 
-    public static String lengthOfPassword(String password){
 
-
-    if (password.length() >= 8){
-        return "Password is valid!";
-    } else {
-        return "Password is invalid!";
-    }
-
-    }
-
-
-
-}
