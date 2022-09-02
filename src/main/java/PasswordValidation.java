52c345aca8d3536fdc0ae5d087aa66750ebffa2f
itsilkerse <itsilkerse@gmail.com>
2022-09-02 10:58:11 +0200

    Length of password is now implemented

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,15 +1,22 @@
 public class PasswordValidation {
 
     public static void main(String[] args) {
+        System.out.println(lengthOfPassword("dal"));
 
+    }
+
+
+    public static String lengthOfPassword(String password){
+
+
+    if (password.length() >= 8){
+        return "Password is valid!";
+    } else {
+        return "Password is invalid!";
+    }
 
     }
 
 
-    public static String lengthOfPassword{
-
-        String.length(6)
-
-    }
 
 }
--- src/test/java/PasswordValidationTest.java
+++ src/test/java/PasswordValidationTest.java
@@ -1,17 +1,42 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordValidationTest {
 
     @Test
-    void passwordLength(){
+    void passwordLengthValid(){
 
         //Given
 
+        String passwordLengthTest = "2232213131";
+
         //When
+
+        String actual = PasswordValidation.lengthOfPassword(passwordLengthTest);
 
         //Then
 
-
+        assertEquals("Password is valid!", actual);
 
     }
+
+
+    @Test
+    void passwordLengthInvalid(){
+
+        //Given
+
+        String passwordLengthTest = "221";
+
+        //When
+
+        String actual = PasswordValidation.lengthOfPassword(passwordLengthTest);
+
+        //Then
+
+        assertEquals("Password is invalid!", actual);
+
+    }
+
 }
